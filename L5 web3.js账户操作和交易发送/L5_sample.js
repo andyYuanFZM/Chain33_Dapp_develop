eadf5f77f2c886f99b0a978ccfaa59a730f815d9
fkeitxflf <fkeitxflf@163.com>
2023-02-22 15:39:06 +0800

    init L6

--- L5 web3.js账户操作和交易发送/L5_sample.js
+++ L5 web3.js账户操作和交易发送/L5_sample.js
@@ -1,9 +1,9 @@
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("http://172.22.16.19:8545"));
+var web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.3.158:8545"));
 
-web3.eth.getAccounts().then(console.log)
+// web3.eth.getAccounts().then(console.log)
 // web3.eth.personal.newAccount('test12345').then(console.log);
-// web3.eth.personal.importRawKey("0x76491916cf0e70437cbed8c2ce9ac2241221e56f8e64ec74e3282b07f24018e1", "genesisKey").then(console.log);
+//web3.eth.personal.importRawKey("0x76491916cf0e70437cbed8c2ce9ac2241221e56f8e64ec74e3282b07f24018e1", "genesisKey").then(console.log);
 // web3.eth.getAccounts().then(console.log)
 // web3.eth.getBalance("0x4797A444f34C26e71803A1d98D5031a3cAE70650").then(console.log)
 
@@ -15,5 +15,5 @@ web3.eth.getAccounts().then(console.log)
 // }
 
 // web3.eth.accounts.signTransaction(transactionOptions, "0x76491916cf0e70437cbed8c2ce9ac2241221e56f8e64ec74e3282b07f24018e1").then(console.log);
-// web3.eth.sendSignedTransaction("").on('receipt', console.log);
-web3.eth.getTransaction("0xb5e8270e1a571851ca85a45fab70d9c5e25d0653f44a0b798050d34cc47243d3").then(console.log)
+// web3.eth.sendSignedTransaction("0xf86f098502540be400831e848094fd89c32962f19bcea69b76093a64a03618cb33be880de0b6b3a7640000808207f2a0ab1a33ffb2179ad7fd19f5ab2cffff07c6333d0b8a905480851dbdfa90151c0fa022b34758fd9e942aad65fddd5d738a08ead59d58ec84299a1fa160b0f97e3849").on('receipt', console.log);
+web3.eth.getTransaction("0xaf35e2873abbb0c2bd641e78b41f08554ab440c8f71b36e1a25ac1e9cfd5f098").then(console.log)
---
+++ L6 web3.js合约部署/L6_sample.js
@@ -0,0 +1,31 @@
+var Web3 = require('web3');
+var fs = require('fs')
+var path = require('path');
+
+var web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.3.158:8545"));
+
+var abi = JSON.parse(fs.readFileSync(path.join(__dirname, "./contract.abi")).toString())
+var bytecode = fs.readFileSync(path.join(__dirname, "./contract.code")).toString()
+
+const deploy = async () => {
+  
+    const contract = new web3.eth.Contract(abi);
+  
+    const contractdeploy = contract.deploy({
+      data: bytecode,
+      arguments: ["init ERC1155 contract"],
+    });
+
+const createTransaction = await web3.eth.accounts.signTransaction(
+    {
+      data: contractdeploy.encodeABI(),
+      gas: await contractdeploy.estimateGas({from: "0x4797A444f34C26e71803A1d98D5031a3cAE70650"}),
+    },
+    "0x76491916cf0e70437cbed8c2ce9ac2241221e56f8e64ec74e3282b07f24018e1"
+  );
+
+const createReceipt = await web3.eth.sendSignedTransaction(createTransaction.rawTransaction);
+console.log(createReceipt);
+}
+
+deploy();
